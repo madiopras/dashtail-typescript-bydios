48922d01aff0c48bebbb023a7b4ed9064565491c
Madio Prasetyo <madiopras@gmail.com>
2024-12-14 14:17:43 +0700

    first commit

---
+++ app/dictionaries.ts
@@ -0,0 +1,14 @@
+import "server-only";
+
+const dictionaries = {
+  id: () => import("./dictionaries/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/en.json").then((module) => module.default),
+  bn: () => import("./dictionaries/bn.json").then((module) => module.default),
+  ar: () => import("./dictionaries/ar.json").then((module) => module.default),
+};
+
+
+
+export const getDictionary = async (locale: keyof typeof dictionaries) => {
+  return dictionaries[locale]();
+};
---
+++ config/site.ts
@@ -0,0 +1,20 @@
+
+
+
+export const siteConfig = {
+  name: "DashTail - Admin By Dios",
+  description: null,
+  theme: "violet",
+  layout: "vertical",
+  // semi-box, horizontal, vertical
+  hideSideBar: false,
+  sidebarType: "module",
+  // popover, classic, module
+  sidebarColor: null,
+  navbarType: "sticky",
+  // sticky, floating, static
+  footerType: "static",
+  // sticky,  static, hidden
+  sidebarBg: "none",
+  radius: 0.5,
+};
